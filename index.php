baa6eb5ca89a8d04355753a36a1e0f80cb4c3d98
ootomoakiyoshi <ootomoakiyoshi@icloud.com>
2022-02-22 12:26:05 +0900

    練習

--- index.php
+++ index.php
@@ -1,15 +1,13 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>PHP</title>
+  <meta name="viewport" content="width=
+  , initial-scale=1.0">
+  <title>練習</title>
 </head>
 <body>
-<?php echo 'Hello, world!'; ?>
-<?php echo 5+2; ?>
-<?php echo "5+2"; ?>
-
+  <?php echo "Hello,wold!"; ?>
 </body>
 </html>
