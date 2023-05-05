1811c1c2841586760c2447316dff0969511fd247
ootomoakiyoshi <ootomoakiyoshi@icloud.com>
2023-05-05 19:04:07 +0900

    add

--- ptest.php
+++ ptest.php
@@ -20,3 +20,30 @@ if($input>=5 && $input<20){
 }
 
 ?>
+
+
+No.22
+<?php
+$input = readline("整数値1");
+if($input<-10 || $input>=10){
+  echo "OK";
+}
+?>
+
+<?php
+$sum = 0;
+for($i = 0; $i <= 5; $i++){
+  $input = readline("整数値");
+  $sum += (int)$input;
+}
+echo $sum;
+?>
+
+<?php
+$input = readline("整数値 ");
+if($input > 0){
+  for($i = 0; $i < $input; $i++){
+    echo "*";
+  }
+}
+?>
